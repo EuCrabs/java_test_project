09643eac4cae71bfe900f9723e8e6a65516d3f46
eucrabs <eucrabs@gmail.com>
2022-03-30 15:50:15 +0300

    Classes

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,12 +1,17 @@
 package com.company;
 
 public class Car {
-    //Variables
+    // Variables
     String model;
     String type;
 
+    // constructor
     Car (String model, String type) {
         this.model = model;
         this.type = type;
+    }
+
+    String sound() {
+        return "Вр.... Вррррр...";
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         Car lada = new Car("Vesta", "SW");
-        System.out.println(lada.model + " " + lada.type);
+        String voice = lada.sound();
+
+        System.out.println("You chose " + lada.model + " " + lada.type + " and it sounds like: " + voice);
     }
 }
