24a9a33ad37d6639944f0b68e945807f7e57a100
eucrabs <eucrabs@gmail.com>
2022-03-30 16:27:27 +0300

    New tests

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Calculator {
+    static double multiply(double a, double b) {
+        return a * b;
+    }
+}
---
+++ src/com/company/CalculatorTest.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.stream.util.XMLEventAllocator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Multiply two numbers")
+    void multiply() {
+//        assertEquals(4, Calculator.multiply(2, 2));
+        assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
+                () -> assertEquals(0, Calculator.multiply(0, 1)),
+                () -> assertEquals(-4, Calculator.multiply(-2, 2)),
+                () -> assertEquals(4, Calculator.multiply(-2, -2)),
+                () -> assertEquals(5, Calculator.multiply(2.5, 2)));
+    }
+}
