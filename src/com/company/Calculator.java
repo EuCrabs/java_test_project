8f57920340d7354df9985752e7d09d6c3ece2770
eucrabs <eucrabs@gmail.com>
2022-03-31 10:11:51 +0300

    Some code

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -2,10 +2,23 @@ package com.company;
 
 public class Calculator {
     static double multiply(double a, double b) {
-        double c = a * b;
-        if (c % 2 == 0) {
-            c = 5;
-        }
-        return c;
+        return a * b;
     }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    static double div(double a, double b) {
+        if (b == 0) {
+            return 0;
+        } else {
+            return a / b;
+        }
+    }
+
+    static double subt(double a, double b) {
+        return a - b;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,14 @@ public class Main {
         Car lada = new Car("Vesta", "SW");
         String voice = lada.sound();
 
-        System.out.println("You chose " + lada.model + " " + lada.type + " and it sounds like: " + voice);
+//        System.out.println("You chose " + lada.model + " " + lada.type + " and it sounds like: " + voice);
+
+        double res = Calculator.add(2,4);
+        double res2 = Calculator.div(5,345);
+        double res3 = Calculator.subt(1,101);
+
+        System.out.println(res);
+        System.out.println(res2);
+        System.out.println(res3);
     }
 }
